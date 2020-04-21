ca0191a4872cb1dbf6c96bc91aaa1595ae032e68
CSRGH27 <universaltaskmail@gmail.com>
2020-04-21 23:08:17 +0200

    Manage dbb

--- src/Migrations/Version20200421210726.php
+++ src/Migrations/Version20200421210726.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200421204238 extends AbstractMigration
+final class Version20200421210726 extends AbstractMigration
 {
     public function getDescription() : string
     {
