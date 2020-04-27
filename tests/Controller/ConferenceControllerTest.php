dd5f62361b7d45bcaac1da5fdb1c16169b80ffc0
CSRGH27 <universaltaskmail@gmail.com>
2020-04-27 23:14:32 +0200

    Installation Symfony panther

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -15,6 +15,34 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+
+
+    /**
+     * Permet de simuler un test de commentaire
+     *
+     * @return void
+     */
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Paul',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.fr',
+            'comment_form[photoFilename]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('h4:contains("Paul")');
+    }
+
+    /**
+     * Permet de simuler une navigation sur une page et un click sur un lien
+     *
+     * @return void
+     */
     public function testConferencePage()
     {
         $client = static::createClient();
@@ -29,4 +57,8 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
         $this->assertSelectorExists('h4:contains("Fabien")');
     }
+
+
+    //TODO  J'ai cree un alias dasn le bash pour effectuer les test rapidement sf-tests  
+
 }
